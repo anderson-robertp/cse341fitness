f4cf00698e49826b1ba35eba7b576e2de74e68fd
Mandi <mandibeauchaine1@gmail.com>
2025-03-19 07:17:16 -0600

    Fix achievement routes to work again.

--- routes/achievements.ts
+++ routes/achievements.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import {
     createAchievement,
     getAchievements,
@@ -9,127 +9,127 @@ import {
 
 const achievementsRouter = express.Router();
 
-//Get Achievements Route
+// Get Achievements Route
 achievementsRouter.get(
     "/",
     getAchievements,
     /*  
-    #swagger.tags = ['Achievements'],
-    #swagger.description = 'Get all achievements.',
+    #swagger.tags = ['Achievements']
+    #swagger.description = 'Retrieve all achievements'
     #swagger.responses[200] = {
-        description: 'Achievements retrieved successfully.',
-    },
+        description: 'Achievements retrieved successfully.'
+    }
     #swagger.responses[500] = {
-        description: 'Error getting achievements.',
+        description: 'Error retrieving achievements.'
     }
     */
 );
 
-//Get Achievement by ID Route
+// Get Achievement by ID Route
 achievementsRouter.get(
     "/:id",
     getAchievementById,
     /*  
-    #swagger.tags = ['Achievements'],
-    #swagger.description = 'Get achievement.',
+    #swagger.tags = ['Achievements']
+    #swagger.description = 'Retrieve an achievement by ID'
     #swagger.parameters['id'] = {
-            "name": "id",
-            "in": "path",
-            "required": true,
-            "type": "string"
-    },
+        "in": "path",
+        "required": true,
+        "type": "string",
+        "description": "The ID of the achievement"
+    }
     #swagger.responses[200] = {
-        description: 'Achievement retrieved successfully.',
-    },
+        description: 'Achievement retrieved successfully.'
+    }
     #swagger.responses[404] = {
-        description: 'Achievement not found.',
-    },
+        description: 'Achievement not found.'
+    }
     #swagger.responses[500] = {
-        description: 'Error retrieving achievement.',
+        description: 'Error retrieving achievement.'
     }
     */
 );
 
-//Create Achievement Route
+// Create Achievement Route
 achievementsRouter.post(
     "/",
     createAchievement,
     /*  
-    #swagger.tags = ['Achievements'],
-    #swagger.description = 'Create a new achievement.',
+    #swagger.tags = ['Achievements']
+    #swagger.description = 'Create a new achievement'
     #swagger.parameters['body'] = {
-            name: 'body',
-            in: 'body',
-            schema: {
-                $title: 'Run 5 miles',
-                $description: 'Run 5 miles in a week',
-                progressGoal: 5
-            }
-    },
-    #swagger.responses[201] = {
-        description: 'Achievement created successfully.',
-    },
-    #swagger.responses[500] = {
-        description: 'Error creating achievement.',
+        "in": "body",
+        "required": true,
+        "schema": {
+            "title": "Run 5 miles",
+            "description": "Run 5 miles in a week",
+            "progressGoal": 5
+        }
     }
-  */
-);
-
-//Delete Achievement Route
-achievementsRouter.delete(
-    "/:id",
-    deleteAchievement,
-    /*  
-    #swagger.tags = ['Achievements'],
-    #swagger.description = 'Delete achievement.',
-    #swagger.parameters['id'] = {
-            "name": "id",
-            "in": "path",
-            "required": true,
-            "type": "string"
-    },
-    #swagger.responses[200] = {
-        description: 'Achievement deleted successfully.',
-    },
-        #swagger.responses[404] = {
-        description: 'Achievement not found.',
-    },
+    #swagger.responses[201] = {
+        description: 'Achievement created successfully.'
+    }
     #swagger.responses[500] = {
-        description: 'Error deleting achievement.',
+        description: 'Error creating achievement.'
     }
     */
 );
 
-//update Achievement Route
+// Delete Achievement Route
+achievementsRouter.delete(
+    "/:id",
+    deleteAchievement,
+    /*  
+    #swagger.tags = ['Achievements']
+    #swagger.description = 'Delete an achievement by ID'
+    #swagger.parameters['id'] = {
+        "in": "path",
+        "required": true,
+        "type": "string",
+        "description": "The ID of the achievement"
+    }
+    #swagger.responses[200] = {
+        description: 'Achievement deleted successfully.'
+    }
+    #swagger.responses[404] = {
+        description: 'Achievement not found.'
+    }
+    #swagger.responses[500] = {
+        description: 'Error deleting achievement.'
+    }
+    */
+);
+
+// Update Achievement Route
 achievementsRouter.put(
     "/:id",
     updateAchievement,
     /*  
-    #swagger.tags = ['Achievements'],
-    #swagger.description = 'Update a new achievement.',
+    #swagger.tags = ['Achievements']
+    #swagger.description = 'Update an existing achievement'
     #swagger.parameters['id'] = {
-            "name": "id",
-            "in": "path",
-            "required": true,
-            "type": "string"
-    },
-    {
-        name: 'body',
-        in: 'body',
-        schema: {
-            $title: 'Run 5 miles',
-            $description: 'Run 5 miles in a week',
-            progressGoal: 5
+        "in": "path",
+        "required": true,
+        "type": "string",
+        "description": "The ID of the achievement"
+    }
+    #swagger.parameters['body'] = {
+        "in": "body",
+        "required": true,
+        "schema": {
+            "title": "Run 5 miles",
+            "description": "Run 5 miles in a week",
+            "progressGoal": 5
         }
-    },
+    }
     #swagger.responses[200] = {
-        description: 'Achievement updated successfully.',
-    },
-        #swagger.responses[404] = {
-        description: 'Achievement not found.',
-    },
+        description: 'Achievement updated successfully.'
+    }
+    #swagger.responses[404] = {
+        description: 'Achievement not found.'
+    }
     #swagger.responses[500] = {
-        description: 'Error updating achievement.',
+        description: 'Error updating achievement.'
     }
     */
 );
